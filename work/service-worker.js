d8eb56d8166e2afbe855873538e30b24efe9f79b
shengchuang-wong <shengchuang1996@gmail.com>
2019-12-31 00:43:02 +0800

    finish debugging

--- work/service-worker.js
+++ work/service-worker.js
@@ -12,10 +12,50 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+const CACHE_NAME = 'my-site-cache-v1'
+const urlsToCache = [
+  '/',
+  '/styles/main.css',
+  '/scripts/main.js',
+  '/images/smiley.svg'
+]
+
 self.addEventListener('install', function(event) {
-  console.log('Service Worker installing.');
-});
+  console.log('A *new* Service Worker is installing.')
+
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache)
+      }).catch(e => {
+        console.log(e)
+      })
+  )
+})
 
 self.addEventListener('activate', function(event) {
-  console.log('Service Worker activating.');  
-});
+  console.log('Finally active. Ready to start serving content!')  
+})
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request)
+      })
+  )
+})
+
+self.addEventListener('push', function(event) {
+  const title = 'Yay a message'
+  const body = 'We have received a push message.'
+  const icon = '/images/smiley.svg'
+  const tag = 'simple-push-example-tag'
+
+  event.waitUntil(
+    self.registration.showNotification(title, {
+      body, icon, tag
+    })
+  )
+})
